6385be8e30012b1f4f109cf85949e08e3d6a0672
Arthur Akvelon <artur.mikhaylov@akvelon.com>
2019-07-03 15:29:42 +0300

    Routes fix

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,23 +2,7 @@ import Vapor
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    // Basic "It works" example
-    router.get { req in
-        return "It works!"
-    }
     
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
-    }
-
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
-    
-    // Example of configuring a controller
     let peopleController = PersonController()
     router.get("people", use: peopleController.list)
     router.get("people", Int.parameter, use: peopleController.single)
@@ -28,5 +12,5 @@ public func routes(_ router: Router) throws {
     let debtsController = DebtsController()
     router.get("people", Int.parameter, "debts", use: debtsController.list)
     router.post("people", Int.parameter, "debts", use: debtsController.create)
-    
+    router.delete("debts", Debt.parameter, use: debtsController.delete)
 }
